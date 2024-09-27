30b59b8b85a86f17952287c2ecb16ebfc50f968f
cnmzsjbz199328 <tangjiang199328@gmail.com>
2024-09-27 23:25:48 +0930

    new edition adapt to mobile phones

--- slang-sharing-app/src/config.js
+++ slang-sharing-app/src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
-//const localApiHost = 'http://localhost/slang-sharing-backend/api';localApiHost || 
+const localApiHost = 'http://localhost/slang-sharing-backend/api';
 const remoteApiHost = process.env.REACT_APP_API_HOST;
 
 const config = {
-    apiHost: remoteApiHost,
+    apiHost: remoteApiHost || localApiHost,
 };
 
 console.log('API Host:', config.apiHost); // 检查当前使用的 apiHost
